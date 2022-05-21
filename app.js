9ac5d6a43429872d0115103a39de1ae7bdaaa4a3
Alexeyenko Artyom <i.html06@gmail.com>
2022-05-21 09:30:29 +0600

    feat: add todo list part 2

--- app.js
+++ app.js
@@ -12,11 +12,28 @@ const App = {
             this.inputValue = e.target.value
         },
         addNewNote() {
-            this.notes.push(this.inputValue)
-            this.inputValue = ''
+            if(this.inputValue !== '') {
+                this.notes.push(this.inputValue)
+                this.inputValue = ''
+            }
+        },
+        toUpperCase(item) {
+            return item.toUpperCase()
         },
         removeNote(idx) {
             this.notes.splice(idx, 1)
+        }
+    },
+    computed: {
+        doubleCountComputed() {
+            return this.notes.length * 2
+        },
+    },
+    watch: {
+        inputValue(value) {
+            if(value.length > 50) {
+                this.inputValue = ''
+            }
         }
     }
 }
